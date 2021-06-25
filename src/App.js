07ca6dca729a7039654e4561d26acf69059e2a28
Joe Joyave <jjoyave@tillster.com>
2021-06-25 15:00:30 -0400

    Feat: Add component for table, load data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { CircularProgress } from '@material-ui/core';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -11,27 +11,65 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
-const useStyles = makeStyles({
-	table: {
-		minWidth: 650,
-	},
-});
-
 const API_URL = 'https://challenge.nfhsnetwork.com/v2/search/events/upcoming?state_association_key=18bad24aaa&amp;card=true&amp;size=50&amp;start=0';
 
-export default function App() {
-	const classes = useStyles();
-	let rows = [];
+class EventTable extends React.Component {
 
-	fetch(API_URL)
-		.then((res) => res.json())
-		.then((result => {
-			console.log(result);
-			rows = rows.concat(result.items);
-		}));
+	constructor(props) {
+		super(props);
+		this.state = {
+			isLoaded: false,
+			items: []
+		}
+	}
 
-	return (
-		<TableContainer component={Paper}>
+	componentDidMount() {
+		fetch(API_URL)
+			.then((res) => res.json())
+			.then((result => {
+				this.setState({
+					isLoaded: true,
+					items: extractData(result.items)
+				})
+			}));
+
+		function extractData(events) {
+			events = events || [];
+			return events.map(e => {
+				const isSportEvent = e.level && e.sport;
+
+				let subheadline = '';
+				const teams = e.teams || [];
+				for (let i=0, iLen = teams.length; i < iLen; i++) {
+					const team = teams[i];
+					subheadline += team.name;
+					if (iLen > 1 && i === 0) {
+						subheadline += ' vs. ';
+					}
+				}
+				return {
+					key: e.key,
+					headline: isSportEvent ? `${e.level} ${e.sport}` : e.activity_name,
+					subheadline: subheadline,
+					date: e.local_start_time
+				}
+			});
+		}
+	}
+
+	render() {
+		const classes = makeStyles({
+			table: {
+				minWidth: 650,
+			},
+		});
+
+		const { isLoaded, items } = this.state;
+
+
+		let rows = items;
+		return isLoaded ? (
+			<TableContainer component={Paper}>
 			<Table className={classes.table} aria-label="simple table">
 			<TableHead>
 				<TableRow>
@@ -43,7 +81,7 @@ export default function App() {
 			</TableHead>
 			<TableBody>
 				{rows.map((row) => (
-					<TableRow key={row.id}>
+					<TableRow key={row.key}>
 						<TableCell component="th" scope="row">
 							{row.key}
 						</TableCell>
@@ -55,5 +93,12 @@ export default function App() {
 			</TableBody>
 			</Table>
 		</TableContainer>
+		) : ( <CircularProgress />)
+	}
+}
+
+export default function App() {
+	return (
+		<EventTable/>
 	);
 }
